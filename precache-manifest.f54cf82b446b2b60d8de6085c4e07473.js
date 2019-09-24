2444deeda3ed78169e524e46219a79ec9fa17974
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-24 11:45:05 +0100

    Updates

---
+++ precache-manifest.f54cf82b446b2b60d8de6085c4e07473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b9b292b7795a50362198f289efce5e",
+    "url": "/react-component-simpsons-quotes/index.html"
+  },
+  {
+    "revision": "f3f4a428b6cf701c481e",
+    "url": "/react-component-simpsons-quotes/static/css/main.3f81fcc1.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react-component-simpsons-quotes/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "f3f4a428b6cf701c481e",
+    "url": "/react-component-simpsons-quotes/static/js/main.03b3e7ca.chunk.js"
+  },
+  {
+    "revision": "e87dc59d15cd9f86c37e",
+    "url": "/react-component-simpsons-quotes/static/js/runtime~main.43db8647.js"
+  }
+]);
